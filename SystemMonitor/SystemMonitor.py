43408b93dbd5beec9e7fe149a526d305549b4738
Parker Fink <parkeririshgod@gmail.com>
2024-06-06 12:26:53 -0400

    can now monitor ram useage

--- SystemMonitor/SystemMonitor.py
+++ SystemMonitor/SystemMonitor.py
@@ -8,13 +8,19 @@ import time
 def getCPU():
     while True:
         time.sleep(1)
-        percent = psutil.cpu_percent()
-        label_cpu.config(text=percent)
-        
+        cpu_percent = psutil.cpu_percent()
+        label_cpu.config(text=cpu_percent)
+
+def getRAM():
+    while True:
+        time.sleep(1)
+        ram_percent = psutil.virtual_memory()[2]
+        label_ram.config(text=ram_percent)
+
         
 #threads
 cpuThread = threading.Thread(target=getCPU)
-
+ramThead = threading.Thread(target=getRAM)
 
 
 
@@ -24,15 +30,17 @@ window.geometry('800x500')
 window.title("System Monitor")
 
 global label_cpu
+global label_ram
 
 cpuThread.start()
-
+ramThead.start()
 
 
 label_cpu = tkinter.Label(text="CPU N/A")
 label_cpu.pack()
 
 label_cpu_temp = tkinter.Label(text="CPU Temp N/A")
+label_cpu_temp.pack()
 
 label_ram = tkinter.Label(text="RAM N/A")
 label_ram.pack()
