6a57a2b7461056a158deeb516fca7f5c1f2c600e
Parker Fink <parkeririshgod@gmail.com>
2024-07-01 22:20:37 -0400

    comments
    -comments and started battery code

--- SystemMonitor/SystemMonitor.py
+++ SystemMonitor/SystemMonitor.py
@@ -18,10 +18,13 @@ def getSysInfo():
         ram_percent = psutil.virtual_memory()[2]
         label_ram.config(text="RAM Usage: " + str(ram_percent) + "%")
 
+        battery = psutil.sensors_battery()
+
         cpuTemp = psutil.sensors_temperatures()
         for a,b in cpuTemp.items():
             for x in b:
                 label_cpu_temp.config(text= "CPU Temp: " + str(x.current) + 'c')
+
 
         gpus = GPUtil.getGPUs()
         for gpu in gpus:
@@ -37,17 +40,19 @@ secondaryThread = threading.Thread(target=getSysInfo)
 
 #main window
 window = tkinter.Tk()
-window.geometry('275x200')
+window.geometry('275x300')
 window.title("System Monitor")
 
 global label_cpu
 global label_ram
 global label_cpu_temp
 global label_gpu
-
+global label_battery
 
 secondaryThread.start()
 
+
+#cpu
 l1 = tkinter.Label(text="CPU:")
 l1.grid(row=0, column=0)
 
@@ -60,6 +65,10 @@ label_cpu.grid(row=2, column=1)
 label_cpu_temp = tkinter.Label(text= "CPU Temp N/A")
 label_cpu_temp.grid(row=3, column=1)
 
+
+
+
+#RAM
 l2 = tkinter.Label(text="RAM:")
 l2.grid(row=4, column=0)
 
@@ -67,11 +76,26 @@ label_ram = tkinter.Label(text= "RAM N/A")
 label_ram.grid(row=5, column=1)
 
 
+
+#graphics card
 l3 = tkinter.Label(text="GPU:")
 l3.grid(row=6, column=0)
 
 label_gpu = tkinter.Label(text= "GPU N/A")
 label_gpu.grid(row=7, column=1)
+
+
+
+
+#battery
+l4 = tkinter.Label(text="Battery:")
+l4.grid(row=8, column=0)
+
+
+
+#percent
+#time left
+#plugged in
 
 
 window.mainloop()
